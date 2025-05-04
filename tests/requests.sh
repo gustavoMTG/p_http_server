8123f173ff4e6c834c6e27948ea38cd4628d7bc2
Gustavo Tomsic <gusmat109@gmail.com>
2025-05-04 15:35:00 -0300

    Add more tests

--- tests/requests.sh
+++ tests/requests.sh
@@ -1,1 +1,3 @@
 curl -H "Connection: keep-alive" -X GET http://localhost:8080/makefile
+curl -H "Connection: keep-alive" -X GET http://localhost:8080/README.md
+curl -H "close" -X GET http://localhost:8080/favicon.ico
